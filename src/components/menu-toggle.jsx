f51cb7232d19802ef47a60c971797b115fbc41a9
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-02-27 22:02:40 -0300

    Create the button toggleMenu with the use of useState.

--- src/components/menu-toggle.jsx
+++ src/components/menu-toggle.jsx
@@ -1,22 +1,38 @@
 import { RiMenu2Line, RiMenu3Line } from "react-icons/ri";
+// Apply the useState of React
+import React, {useState} from "react";
 
 
 const MenuToggle = () => {
 
+  const [isShow, setIsShow] = useState(true)
+  const [isList, setIsList] = useState(true)
+
+  const handleClick = () => {
+    setIsShow(!isShow);
+  };
+
+  const handleListClick = () => {
+    setIsList(!isList)
+  }
 
   let openMenu = < RiMenu2Line className="text-gray-200 size-10 cursor-pointer absolute right-0 mx-4" title="Open menu"/>;
   let closeMenu = < RiMenu3Line className="text-gray-200 size-10 cursor-pointer absolute right-0 mx-4" title="Close menu" />;
 
-
   return (
-    <div className="bg-blue-500 w-full h-16 p-4 relative">
-      <select className="bg-gray-300 text-blue-800 font-bold text-lg inline-block w-48 p-2 absolute right-2 top-4 rounded text-center uppercase" >
-        <option className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Home</option>
-        <option className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">About me</option>
-        <option className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Stack</option>
-        <option className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Projects</option>
-        <option className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Interest</option>
-     </select>
+    <div className="bg-blue-500 w-full h-16 p-4 relative" onClick={handleListClick}> 
+      {/* {isShow ? openMenu : closeMenu} */}
+
+      {
+      isList ? openMenu :
+      <ul className="bg-gray-300 text-blue-800 font-bold text-lg inline-block w-56 p-2 absolute right-2 top-20 rounded text-center uppercase" >
+        <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Home</li>
+        <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">About me</li>
+        <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Stack</li>
+        <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Projects</li>
+        <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Interest</li>
+     </ul>
+      }
     </div>
   )
 }
