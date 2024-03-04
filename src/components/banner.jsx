c2b60701891c71bbd271757de20b0e318acff092
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-03-04 05:53:46 -0300

    Apply md:w-60 in layouts less that 768px.

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
   return (
     
     <div className="mx-20 my-4">
-      <h1 className="text-3xl font-extrabold italic text-indigo-800 m-4 drop-shadow-lg">
+      <h1 className="my-4 text-3xl font-extrabold italic text-indigo-800 drop-shadow-lg">
         I'm Wesley Silva!
       </h1>
       <h2 className="text-lg text-wrap font-bold italic text-indigo-500 mx-10">
--- src/components/menu-toggle.jsx
+++ src/components/menu-toggle.jsx
@@ -18,12 +18,12 @@ const MenuToggle = () => {
   let closeMenu = < RiMenu3Line className="text-gray-200 size-10 cursor-pointer absolute right-0 mx-4" title="Close menu" />;
 
   return (
-    <div className="bg-blue-500 w-full h-16 p-4 relative" onClick={handleListClick}> 
+    <div className="bg-blue-500 w-full h-16 p-4 relative z-0" onClick={handleListClick}> 
       {
       isList ? openMenu : closeMenu
       }
       { isShow ? closeMenu :
-        <ul className="up-menu bg-gray-300 text-blue-800 font-bold text-lg w-56 p-2 absolute right-2 top-20 rounded text-center uppercase" >
+        <ul className="up-menu bg-gray-300 text-blue-800 font-bold text-lg w-56 p-2 absolute z-10 right-0 top-20 rounded text-center uppercase max-sm:w-60" >
         <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Home</li>
         <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">About me</li>
         <li className="hover:cursor-pointer hover:bg-gray-400 rounded px-12">Stack</li>
