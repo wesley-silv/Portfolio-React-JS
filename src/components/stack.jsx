e4873116f6ed8facb7e75222520825a183fe374a
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-03-05 06:57:58 -0300

    Increment others use states to icons, style the containers of stack.

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -1,31 +1,41 @@
 import { IoLogoHtml5, IoLogoCss3, IoLogoJavascript, IoLogoReact, IoLogoNodejs } from "react-icons/io5";
-import { SiTailwindcss, SiBootstrap } from "react-icons/si";
+import { SiTailwindcss, SiBootstrap, SiJquery } from "react-icons/si";
 
 import Reaact, {useState} from 'react'
 
 const Stack = () => {
 
   const [isReact, setIsReact] = useState(true)
+  const [isJavaScript, setIsJavaScript] = useState(true)
 
   const handleReact = () => {
-    setIsReact(!isReact)
+    setIsReact(!isReact)    
+  }
+  const handleJavaScript = () => {
+    setIsJavaScript(!isJavaScript)
   }
 
   return (
     <div >
-      <section className="mx-16 m-20 text-4xl flex">
+      <section className="mx-16 m-24 text-4xl flex">
       <IoLogoHtml5 className="text-red-500 cursor-pointer m-1"/>
       <IoLogoCss3 className="text-blue-500 cursor-pointer m-1"/>
-      <IoLogoJavascript className="text-yellow-500 cursor-pointer m-1"/>
+      <SiBootstrap className="text-blue-700 cursor-pointer m-1"/>
+      <SiTailwindcss className="text-blue-400 cursor-pointer m-1"/>
+      <IoLogoJavascript className="text-yellow-500 cursor-pointer m-1" onClick={handleJavaScript}/>
+      <SiJquery className="text-blue-600 cursor-pointer m-1"/>
       <IoLogoReact className="text-blue-500 cursor-pointer m-1" onClick={handleReact}/>
       <IoLogoNodejs className="text-green-500 cursor-pointer m-1"/>
-      <SiTailwindcss className="text-blue-600 cursor-pointer m-1"/>
-      <SiBootstrap className="text-blue-700 cursor-pointer m-1"/>
       </section>
-      <article className="absolute top-64 m-16 p-4 bg-gray-200 h-60 w-80 rounded-md shadow-md">
+      <article className="absolute top-64 my-16 mx-8 p-4 flex">
       {
-        isReact ? <p className="drop-shadow-lg text-xl">Touch in icon to see your content</p> : <article className="block drop-shadow-lg text-xl">
-          With the React, I can create fantastic fronts 
+        isReact ? <p className="drop-shadow-lg text-xl"></p> : <article className=" h-60 w-96 block text-blue-700 bg-gray-200 rounded-md shadow-lg drop-shadow-md p-4 text-center text-lg">
+          React is a fantastic framework! I use to create responsive fronts, structure components in displays, insert icons, change of states that management dynamic interactions to user, and many more. 
+        </article>
+      }
+      {
+        isJavaScript ? <p className="drop-shadow-lg text-xl"></p> : <article className=" h-60 w-96 block text-black bg-yellow-200 rounded-md shadow-lg drop-shadow-md p-4 text-center text-lg">
+          JavaScript is a language of programation that use to create my projects, her can be in different integrations such as, creations of API, apply dynamism in the interactions of user with display, activete mechanism of serch in browsers, do query in databases between others use. 
         </article>
       }
       </article>
