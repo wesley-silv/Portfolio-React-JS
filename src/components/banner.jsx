cda6e5a3e873c043e9e582fd23a8b5eb669490cc
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-03-09 13:07:37 -0300

    Update banner.jsx

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,13 +1,25 @@
-const Banner = () => {
+import React, {useState, useEffect} from "react"
+
+const Banner = ({words, time}) => {
+
+  const [indexWord, setIndexWord] = useState(0)
+
+  useEffect(() => {
+    const defineInterval = setInterval(() => {
+      setIndexWord((prevIndice) => (prevIndice + 1) % words.length)
+    }, time)
+    
+    return () => clearInterval(defineInterval)
+  }, [words, time])
 
   return (
     
     <div className="mx-28 mb-12">
-      <h2 className="mb-4 text-4xl font-extrabold italic text-indigo-800 drop-shadow-lg">
-        I'm Wesley Silva!
-      </h2>
-      <h2 className="text-lg font-bold italic text-indigo-500 mx-10">
-        Welcome to my Portfolio, here you will can find all content about me.
+      <h1 className="mb-4 text-5xl font-extrabold italic text-indigo-800 drop-shadow-lg">
+      {words[indexWord]}
+      </h1>
+      <h2 className="text-2xl font-bold italic text-indigo-500 mx-10">
+        Navigate for all available content!
       </h2>
     </div>
   )
