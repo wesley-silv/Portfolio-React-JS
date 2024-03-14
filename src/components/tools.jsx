f167512f1f25c8a1a8c3255510441dd64c58d9df
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-03-13 21:34:36 -0300

    Align the contant of Tools with flex justify-between.

--- src/components/tools.jsx
+++ src/components/tools.jsx
@@ -13,38 +13,39 @@ const Tools = () => {
       <h3 className="text-center text-xl text-blue-900 font-bold pb-16">
       Each of them provides better performance for the project.
       </h3>
-      <section className="flex space-x-32">
-        <article className="">
-          <p className="text-center text-xl text-orange-600 font-bold pb-16">
+
+      <section className="flex justify-between uppercase">
+        <article >
+          <p className="text-xl text-orange-600 font-bold pb-16">
             Code monitoring 
           </p>
-          <FaGitAlt className="text-[#F05032] m-2 size-12 max-sm:size-24" />
-          <FaSquareGithub className="text-[#181717] m-2 size-12 max-sm:size-24" />
-          <FaBitbucket className="text-[#0052CC] m-2 size-12 max-sm:size-24" />
+          <FaGitAlt className="text-[#F05032] my-3 mx-16 size-12 max-sm:size-24" />
+          <FaSquareGithub className="text-[#181717] my-3 mx-16 size-12 max-sm:size-24" />
+          <FaBitbucket className="text-[#0052CC] my-3 mx-16 size-12 max-sm:size-24" />
         </article>
-        <article>
-          <p className="text-center text-xl text-orange-600 font-bold pb-16">
+        <article >
+          <p className="text-xl text-orange-600 font-bold pb-16">
             APIs Connection 
           </p>
-          <FaNpm className="text-[#CB3837] m-2 size-12 max-sm:size-24" />
-          <SiExpress className="text-[#000000] m-2 size-12 max-sm:size-24" />
-          <SiPostman className="text-[#DD3A0A] m-2 size-12 max-sm:size-24" />
-          <SiHeroku className="text-[#430098] ray-500 m-2 size-12 max-sm:size-24" />
+          <FaNpm className="text-[#CB3837] my-3 mx-16 size-12 max-sm:size-24" />
+          <SiExpress className="text-[#000000] my-3 mx-16 size-12 max-sm:size-24" />
+          <SiPostman className="text-[#DD3A0A] my-3 mx-16 size-12 max-sm:size-24" />
+          <SiHeroku className="text-[#430098] ray-500 my-3 mx-16 size-12 max-sm:size-24" />
         </article>
-        <article>
-          <p className="text-center text-xl text-orange-600 font-bold pb-16">
+        <article >
+          <p className="text-xl text-orange-600 font-bold pb-16">
             Database integration
           </p>
-          <SiMongodb className="text-[#47A248] m-2 size-12 max-sm:size-24" />
-          <SiMysql className="text-[#4479A1] m-2 size-12 max-sm:size-24" />
-          <BiLogoPostgresql className="text-[#4169E1] m-2 size-12 max-sm:size-24" />
+          <SiMongodb className="text-[#47A248] my-3 mx-16 size-12 max-sm:size-24" />
+          <SiMysql className="text-[#4479A1] my-3 mx-16 size-12 max-sm:size-24" />
+          <BiLogoPostgresql className="text-[#4169E1] my-3 mx-16 size-12 max-sm:size-24" />
         </article>
-        <article>
-          <p className="text-center text-xl text-orange-600 font-bold pb-16">
+        <article >
+          <p className="text-xl text-orange-600 font-bold pb-16">
             Software management
           </p>
-          <FaConfluence className="text-[#172B4D] m-2 size-12 max-sm:size-24" />
-          <FaJira className="text-[#0052CC] m-2 size-12 max-sm:size-24" />
+          <FaConfluence className="text-[#172B4D] my-3 mx-16 size-12 max-sm:size-24" />
+          <FaJira className="text-[#0052CC] my-3 mx-16 size-12 max-sm:size-24" />
         </article>
       </section>
     </div>
