be5d5601f89313b104aee208365383b3871f4009
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-04-16 10:00:32 -0300

    style(app.jsx menu-toggle.jsx): change of styles in the same components, better responsivity view

--- src/components/menu-toggle.jsx
+++ src/components/menu-toggle.jsx
@@ -11,11 +11,18 @@ const MenuToggle = () => {
     setIsShow(!isShow)
   }
 
+  const headerScroll = () => {
+    objHeader = document.getElementById('header')
+    if(objHeader == window.screenY <= 0){
+      objHeader.style.positon = '#fixed'
+    }
+  }
+
   let openMenu = < RiMenu2Line className="text-gray-200 size-10 cursor-pointer absolute right-0 mx-4" onClick={handleListClick}/>;
   let closeMenu = < RiMenu3Line className="text-gray-200 size-10 cursor-pointer absolute right-0 mx-4" onClick={handleListClick}/>;
 
   return (
-    <div className="bg-gradient-to-r from-blue-700 to-blue-500 w-full h-16 p-3 relative"> 
+    <div className="bg-gradient-to-r from-blue-700 to-blue-500 w-full h-16 p-3 relative" id="header" > 
       {
       isList ? openMenu : closeMenu
       }
