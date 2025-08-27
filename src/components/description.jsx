315a3a36a3092ecd60a726e9ee8b2e0f2b3d2b01
Wesley Silva <wesleysilvaconceicao@outlook.com>
2025-08-27 01:43:36 -0300

    refactor: adding a new image and reestructure the icons and descriptions

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { FaGraduationCap, FaBriefcase, FaLaptopCode } from 'react-icons/fa';
+import { FaGraduationCap, FaBriefcase, FaLaptopCode, FaLanguage } from 'react-icons/fa';
 
 const Description = () => {
   return (
     <div className="my-24 mx-8">
       <section className="flex flex-row max-lg:flex-col justify-between items-center max-lg:mx-4 gap-8">
         <article className="bg-gray-100 flex flex-col items-center shadow-lg rounded-lg p-8 m-4 max-lg:mx-auto md:w-full lg:w-[48%]">
-          <div className="relative p-1 rounded-lg border-2 border-blue-600 shadow-lg overflow-hidden">
+          <div className="relative p-1 rounded-lg overflow-hidden">
             <img
-              src="/Wesley S. Conceição.jpg"
-              alt="Wesley da Silva Conceição"
-              className="rounded-lg object-cover w-52 h-52 hover:scale-105 transition-transform duration-300 ease-in-out"
+              src="/Wesley Silva Conceição.png"
+              alt="Wesley Silva"
+              className="rounded-lg object-cover w-60 h-60"
             />
           </div>
           <h4 className="mt-6 text-blue-700 text-2xl font-bold text-center">
-            Wesley da Silva Conceição
+            Wesley Silva
           </h4>
           <p className="text-center text-lg">
             <span className="text-blue-900 font-semibold">
-              Ciência da Computação
+              Desenvolvedor Front-end
             </span><br />
-            Universidade Anhembi Morumbi - SP
+            Ciência da Computação UAM/SP
           </p>
         </article>
 
@@ -36,8 +36,7 @@ const Description = () => {
             soluções dinâmicas e intuitivas.
           </p>
           <p className="leading-relaxed mt-4 text-gray-700">
-            <FaGraduationCap className="inline-block mr-2 size-5 text-blue-700" />
-            Formação contínua em tecnologias modernas:
+            Formação contínua em tecnologias modernas com
             <span className="text-blue-900 font-semibold"> Rocketseat </span>
             e
             <span className="text-blue-900 font-semibold"> Alura</span>.
@@ -79,6 +78,7 @@ const Description = () => {
             Desenvolvimento front-end de interfaces e e-commerce, com foco na
             experiência do usuário e otimização de performance.
             <br />
+            <FaBriefcase className="inline-block mr-2 size-5 text-blue-700" />
             <span className="text-blue-900 font-semibold">Gerenciamento:</span>{" "}
             Manutenção de redes e gerenciamento de radares de tráfego.
           </p>
@@ -86,16 +86,15 @@ const Description = () => {
 
         <article className="bg-gray-100 shadow-lg rounded-lg p-8 m-4 max-lg:mx-auto lg:w-[48%]">
           <h4 className="mb-4 text-blue-700 text-2xl font-bold text-center">
-            Estudos
+            Acadêmico
           </h4>
           <p className="leading-relaxed text-gray-700">
-            <FaGraduationCap className="inline-block mr-2 size-5 text-blue-700" />
-            <span className="text-blue-900 font-semibold">Graduando</span> em
-            Ciência da Computação, focado em ampliar conhecimentos técnicos e
-            aplicabilidade prática.
+            <FaGraduationCap className="inline-block mr-2 size-7 text-blue-700" />
+            <span className="text-blue-900 font-semibold">Graduação</span> em
+            Ciência da Computação, focado em ampliar conhecimentos tecnológicos específicos.
             <br />
-            <span className="text-blue-900 font-semibold">Inglês:</span> Nível
-            técnico para leitura e interpretação de documentos de desenvolvimento.
+            <FaLanguage className="inline-block mr-2 size-8 text-blue-700" />
+            <span className="text-blue-900 font-semibold">Inglês </span> avançado para comunicação efetiva e colaboração com equipes no ambiente de desenvolvimento de aplicações.
           </p>
         </article>
       </section>
